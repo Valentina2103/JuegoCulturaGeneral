23484f2bd5f11773924e874e9543ed87221d5045
Valentina Penuela Navas <valentina.penuela.2021@upb.edu.co>
2021-11-05 20:16:08 -0500

    Agregar archivos de proyecto.

---
+++ JuegoCulturaGeneral/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace JuegoCulturaGeneral
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Silver;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Monotype Corsiva", 34.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(-7, -2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(517, 74);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.UseWaitCursor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Silver;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 28.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(103, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(296, 57);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Cultura General";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(203, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 24);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 24);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "label2";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(16, 193);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(123, 28);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(16, 264);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(123, 28);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(16, 339);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(123, 28);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(16, 386);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(469, 279);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(166, 686);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 53);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Siguiente";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 803);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ JuegoCulturaGeneral/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JuegoCulturaGeneral
+{
+    public partial class Form1 : Form
+    {
+        int resultado = 0;
+        int seleccion = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Pregunta1();
+        }
+        private void Pregunta1()
+        {
+            if (MessageBox.Show("Bienvenida/o a un pequeño juego que te dara a conocer que tanto sabes sobre cultura general", "Cultura General 🌎", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                this.label1.Text = "Pregunta 1";
+                this.label2.Text = "¿Cuál es el océano más grande del mundo?";
+                this.radioButton1.Text = "Océano Pacífico";
+                this.radioButton2.Text = "Océano Índico";
+                this.radioButton3.Text = "Océano Atlántico";
+                Image img = Image.FromFile("../../images/oceano.jpg");
+                pictureBox1.Image = img;
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+        private void Pregunta2()
+        {
+            this.label1.Text = "Pregunta 2";
+            this.label2.Text = "¿Cuál es el nombre de especie de los seres humanos?";
+            this.radioButton1.Text = "Homo sapiens";
+            this.radioButton2.Text = "Homo sapiens sapiens";
+            this.radioButton3.Text = "Homo erectus";
+            Image img = Image.FromFile("../../images/homo.jpg");
+            pictureBox1.Image = img;
+            seleccion++;
+        }
+        private void Pregunta3()
+        {
+            this.label1.Text = "Pregunta 3";
+            this.label2.Text = "¿Cuál es el animal más grande de la Tierra?";
+            this.radioButton1.Text = "La orca";
+            this.radioButton2.Text = "La ballena azul";
+            this.radioButton3.Text = "La ballena gris";
+            Image img = Image.FromFile("../../images/animales1.jpg");
+            pictureBox1.Image = img;
+            seleccion++;
+        }
+
+        private void Pregunta4()
+        {
+            this.label1.Text = "Pregunta 4";
+            this.label2.Text = "¿Cuánto duró “La Guerra de los Cien Años”?";
+            this.radioButton1.Text = "100 años";
+            this.radioButton2.Text = "200 años";
+            this.radioButton3.Text = "116 años";
+            Image img = Image.FromFile("../../images/100.jpg");
+            pictureBox1.Image = img;
+            seleccion++;
+        }
+        private void Pregunta5()
+        {
+            this.label1.Text = "Pregunta 5";
+            this.label2.Text = "¿Cuál es el país más grande?";
+            this.radioButton1.Text = "Rusia";
+            this.radioButton2.Text = "Brasil";
+            this.radioButton3.Text = "China";
+            Image img = Image.FromFile("../../images/bolamundo.jpg");
+            pictureBox1.Image = img;
+            seleccion++;
+        }
+        private void Pregunta6()
+        {
+            this.label1.Text = "Pregunta 6";
+            this.label2.Text = "¿Cuál es el videojuego más vendido de la historia?";
+            this.radioButton1.Text = "Grand Theft Auto V";
+            this.radioButton2.Text = "Minecraft";
+            this.radioButton3.Text = "Pac-Man";
+            Image img = Image.FromFile("../../images/videojuego.jpg");
+            pictureBox1.Image = img;
+            seleccion++;
+        }
+        private void Pregunta7()
+        {
+            this.label1.Text = "Pregunta 7";
+            this.label2.Text = "¿Cuál es el metal más caro del mundo?";
+            this.radioButton1.Text = "Oro";
+            this.radioButton2.Text = "Platino";
+            this.radioButton3.Text = "Rodio";
+            Image img = Image.FromFile("../../images/metal.jpg");
+            pictureBox1.Image = img;
+            seleccion++;
+        }
+        private void Pregunta8()
+        {
+            this.label1.Text = "Pregunta 8";
+            this.label2.Text = "¿Cómo se llama el animal más rápido del mundo?";
+            this.radioButton1.Text = "Guepardo";
+            this.radioButton2.Text = "Halcón peregrino";
+            this.radioButton3.Text = "Escarabajo tigre";
+            Image img = Image.FromFile("../../images/ANIMALES.png");
+            pictureBox1.Image = img;
+            seleccion++;
+        }
+        private void Pregunta9()
+        {
+            this.label1.Text = "Pregunta 9";
+            this.label2.Text = "¿Cuál es la cascada más alta del mundo?";
+            this.radioButton1.Text = "El Salto del Ángel";
+            this.radioButton2.Text = "Cataratas de Iguazú";
+            this.radioButton3.Text = "Cataratas del Niágara";
+            Image img = Image.FromFile("../../images/cascada.jpg");
+            pictureBox1.Image = img;
+            seleccion++;
+        }
+        private void Pregunta10()
+        {
+            this.label1.Text = "Pregunta 10";
+            this.label2.Text = "¿Cuál es la obra más importante de la literatura en español?";
+            this.radioButton1.Text = "El Principito";
+            this.radioButton2.Text = "Don Quijote de la Mancha";
+            this.radioButton3.Text = "Cien años de soledad";
+            Image img = Image.FromFile("../../images/libros.jpg");
+            pictureBox1.Image = img;
+            seleccion++;
+        }
+        private void Limpiar()
+        {
+            this.radioButton1.Checked = false;
+            this.radioButton2.Checked = false;
+            this.radioButton3.Checked = false;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.radioButton1.Checked == false &&
+            this.radioButton2.Checked == false &&
+            this.radioButton3.Checked == false)
+            {
+                MessageBox.Show("Debes seleccionar una respuesta", "Alerta", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+            else
+            {
+                switch (seleccion)
+                {
+                    case 1:
+                        if (this.radioButton1.Checked == true)
+                        {
+                            resultado++;
+                        }
+                        Limpiar();
+                        Pregunta2();
+                        break;
+
+                    case 2:
+                        if (this.radioButton2.Checked == true)
+                        {
+                            resultado++;
+                        }
+                        Limpiar();
+                        Pregunta3();
+                        break;
+
+                    case 3:
+                        if (this.radioButton2.Checked == true)
+                        {
+                            resultado++;
+                        }
+                        Limpiar();
+                        Pregunta4();
+                        break;
+
+                    case 4:
+                        if (this.radioButton3.Checked == true)
+                        {
+                            resultado++;
+                        }
+                        Limpiar();
+                        Pregunta5();
+                        break;
+
+                    case 5:
+                        if (this.radioButton1.Checked == true)
+                        {
+                            resultado++;
+                        }
+                        Limpiar();
+                        Pregunta6();
+                        break;
+
+                    case 6:
+                        if (this.radioButton2.Checked == true)
+                        {
+                            resultado++;
+                        }
+                        Limpiar();
+                        Pregunta7();
+                        break;
+
+                    case 7:
+                        if (this.radioButton3.Checked == true)
+                        {
+                            resultado++;
+                        }
+                        Limpiar();
+                        Pregunta8();
+                        break;
+
+                    case 8:
+                        if (this.radioButton1.Checked == true)
+                        {
+                            resultado++;
+                        }
+                        Limpiar();
+                        Pregunta9();
+                        break;
+
+                    case 9:
+                        if (this.radioButton1.Checked == true)
+                        {
+                            resultado++;
+                        }
+                        Limpiar();
+                        Pregunta10();
+                        break;
+
+                    case 10:
+                        if (this.radioButton2.Checked == true)
+                        {
+                            resultado++;
+                        }
+
+                        if (resultado >= 7)
+                        {
+                            MessageBox.Show("Tus respuestas correctas han sido: " + resultado.ToString(), "Felicitaciones 😄", MessageBoxButtons.OK);
+                            Application.Exit();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Tus respuestas correctas han sido: " + resultado.ToString(), "Puedes Mejorar 😬", MessageBoxButtons.OK);
+                            Application.Exit();
+                        }
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+
